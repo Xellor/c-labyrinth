fcde042e7696467315b64612dac38e6c63e9b812
Xellor <xellor.me@gmail.com>
2016-11-04 18:01:49 +0300

    [*]Code clearance

--- main.c
+++ main.c
@@ -49,88 +49,97 @@ int main()
 void printTDArray(const int **array, int size_x, int size_y)
 {
 	int *pArray = (int*)array;
-	for(int i = 0; i < size_x; i++) {
-		for(int j = 0; j < size_y; j++)
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
 			printf("%3d", pArray[i * size_x + j]);
+		}
 		printf("\n");
 	}
 }
 void printCArray(char array[], int size)
 {
-	for(unsigned i = 0; i < size; i++)
+	for (unsigned i = 0; i < size; i++) {
 		printf("%c", array[i]);
+	}
 	printf(";\n");
-}	// check numbers enters and exits
+}
+// check numbers enters and exits
 void checkLabyrinth(int **array, int size_x, int size_y)
 {
 	int *pArray = (int*)array;
 	int numEnter = 0,
 		numExit  = 0;
-	for(int i = 0; i < size_x; i++) {
-		for(int j = 0; j < size_y; j++) {
-			if(pArray[i * size_x + j] == -1)
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			if (pArray[i * size_x + j] == -1) {
 				numEnter++;
-			if(pArray[i * size_x + j] == -2)
+			}
+			if (pArray[i * size_x + j] == -2) {
 				numExit++;
+			}
 		}
 	}
-	if(numEnter == 1 && numExit == 1)
+	if (numEnter == 1 && numExit == 1) {
 		printf("Check is [OK]\n");
+	}
 	else {
-		printf("Check is [ERROR]\nEnters = %d\nExits = %d\n", 
+		printf("Check is [ERROR]\nEnters = %d\nExits = %d\n",
 			numEnter, numExit);
 		exit(1);
 	}
-}	// marks adjacent sections 'pos_x' and 'pos_y' flag 'n'
+}
+// marks adjacent sections 'pos_x' and 'pos_y' flag 'n'
 void mark(int **array, int size_x, int size_y, int n, int pos_x, int pos_y)
 {
 	int *pArray = (int*)array;
-	if(pArray[pos_x * size_x + pos_y + 1] == 1)
+	if (pArray[pos_x * size_x + pos_y + 1] == 1) {
 		pArray[pos_x * size_x + pos_y + 1] = n;
-	if(pArray[pos_x * size_x + pos_y - 1] == 1)
+	}
+	if (pArray[pos_x * size_x + pos_y - 1] == 1) {
 		pArray[pos_x * size_x + pos_y - 1] = n;
-	if(pArray[(pos_x + 1) * size_x + pos_y] == 1)
+	}
+	if (pArray[(pos_x + 1) * size_x + pos_y] == 1) {
 		pArray[(pos_x + 1) * size_x + pos_y] = n;
-	if(pArray[(pos_x - 1) * size_x + pos_y] == 1)
+	}
+	if (pArray[(pos_x - 1) * size_x + pos_y] == 1) {
 		pArray[(pos_x - 1) * size_x + pos_y] = n;
+	}
 }
 void wave(int **array, int size_x, int size_y)	// wave function
 {
 	checkLabyrinth((int**)array, size_x, size_y);
 	int *pArray = (int*) array;
-	int n = 2, m1 = 2, m2 = 0, i, j;
+	int n = 2, i, j;
 
-	for(i = 0; i < size_x; i++) {
-		for(j = 0; j < size_y; j++) {
-			if(pArray[i * size_x + j] == -1) {
+	for (i = 0; i < size_x; i++) {
+		for (j = 0; j < size_y; j++) {
+			if (pArray[i * size_x + j] == -1) {
 				mark((int**)array, size_x, size_y, n, i, j);
 				n++;
-				m1++;
 				break;
 			}
 		}
 	}
-	do {
-		for(i = 0; i < size_x; i++) {
-			for(j = 0; j < size_y; j++) {
-				if(pArray[i * size_x + j] == n - 1) {
+	for (int m1 = 2, m2 = 0; n <= size_x * size_y; m2 == m1, n++) {
+		for (i = 0; i < size_x; i++) {
+			for (j = 0; j < size_y; j++) {
+				if (pArray[i * size_x + j] == n - 1) {
 					mark((int**)array, size_x, size_y, n, i, j);
 					m1++;
 				}
 			}
 		}
-		n++;
-		if(m1 == m2)
+		if (m1 == m2) {
 			break;
-		m2 = m1;
-	} while (n < size_x * size_y);
+		}
+	}
 }	// return nearest section (Right, Up etc)
 char shortest(int **array, int size_x, int size_y, int pos_x, int pos_y)
 {
 	int *pArray = (int*)array;
 	int min = size_x * size_y, min_x, min_y;
 
-	if(pArray[pos_x * size_x + (pos_y + 1)] < min &&
+	if (pArray[pos_x * size_x + (pos_y + 1)] < min &&
 	pArray[pos_x * size_x + (pos_y + 1)] != -2 &&
 	pArray[pos_x * size_x + (pos_y + 1)] != 0 &&
 	pArray[pos_x * size_x + (pos_y + 1)] != 1) {
@@ -138,7 +147,7 @@ char shortest(int **array, int size_x, int size_y, int pos_x, int pos_y)
 		min_x = pos_x;
 		min_y = pos_y + 1;
 	}
-	if(pArray[pos_x * size_x + (pos_y - 1)] < min &&
+	if (pArray[pos_x * size_x + (pos_y - 1)] < min &&
 	pArray[pos_x * size_x + (pos_y - 1)] != -2 &&
 	pArray[pos_x * size_x + (pos_y - 1)] != 0 &&
 	pArray[pos_x * size_x + (pos_y - 1)] != 1) {
@@ -146,7 +155,7 @@ char shortest(int **array, int size_x, int size_y, int pos_x, int pos_y)
 		min_x = pos_x;
 		min_y = pos_y - 1;
 	}
-	if(pArray[(pos_x - 1) * size_x + pos_y] < min &&
+	if (pArray[(pos_x - 1) * size_x + pos_y] < min &&
 	pArray[(pos_x - 1) * size_x + pos_y] != -2 &&
 	pArray[(pos_x - 1) * size_x + pos_y] != 0 &&
 	pArray[(pos_x - 1) * size_x + pos_y] != 1) {
@@ -154,7 +163,7 @@ char shortest(int **array, int size_x, int size_y, int pos_x, int pos_y)
 		min_x = pos_x - 1;
 		min_y = pos_y;
 	}
-	if(pArray[(pos_x + 1) * size_x + pos_y] < min &&
+	if (pArray[(pos_x + 1) * size_x + pos_y] < min &&
 	pArray[(pos_x + 1) * size_x + pos_y] != -2 &&
 	pArray[(pos_x + 1) * size_x + pos_y] != 0 &&
 	pArray[(pos_x + 1) * size_x + pos_y] != 1) {
@@ -162,117 +171,101 @@ char shortest(int **array, int size_x, int size_y, int pos_x, int pos_y)
 		min_x = pos_x + 1;
 		min_y = pos_y;
 	}
-	if(min_x == pos_x && min_y == pos_y + 1)
+	if (min_x == pos_x && min_y == pos_y + 1) {
 		return 'R';
-	else if(min_x == pos_x && min_y == pos_y - 1)
+	} else if (min_x == pos_x && min_y == pos_y - 1) {
 		return 'L';
-	else if(min_x == pos_x - 1 && min_y == pos_y)
+	} else if (min_x == pos_x - 1 && min_y == pos_y) {
 		return 'U';
-	else if(min_x == pos_x + 1 && min_y == pos_y)
+	} else if (min_x == pos_x + 1 && min_y == pos_y) {
 		return 'D';
-	else return '\0';
-}	// function way back
+	} else {
+		return '\0';
+	}
+}
+// function way back
 void back(int **array, int size_x, int size_y, char *way, int sizeWay)
 {
 	int *pArray = (int*)array;
 	int i, j, enter_x, enter_y, exit_x, exit_y;
 
-	for(i = 0; i < size_x; i++) {
-		for(j = 0; j < size_y; j++) {
-			if(pArray[i * size_x + j] == -1) {	// find enter
+	for (i = 0; i < size_x; i++) {
+		for (j = 0; j < size_y; j++) {
+			if (pArray[i * size_x + j] == -1) {	// find enter
 				enter_x = i;
 				enter_y = j;
 			}
-			if(pArray[i * size_x + j] == -2) {	// find exit
+			if (pArray[i * size_x + j] == -2) {	// find exit
 				exit_x = i;
 				exit_y = j;
 			}
 		}
 	}
-
-	i = 0;
 	int back_x = exit_x;
 	int back_y = exit_y;
 	way[0] = shortest((int**)array, size_x, size_y, exit_x, exit_y);
 
-	do {
-		if(back_x == enter_x && back_y == enter_y) {
+	for (i = 0; i < size_x * size_y; i++) {
+		if (back_x == enter_x && back_y == enter_y) {
 			break;
-		} else if(way[i] == 'R') {
+		} else if (way[i] == 'R') {
 			back_y = back_y + 1;
 			way[i + 1] = shortest((int**)array, size_x, size_y, back_x, back_y);
-		} else if(way[i] == 'L') {
+		} else if (way[i] == 'L') {
 			back_y = back_y - 1;
 			way[i + 1] = shortest((int**)array, size_x, size_y, back_x, back_y);
-		} else if(way[i] == 'U') {
+		} else if (way[i] == 'U') {
 			back_x = back_x - 1;
 			way[i + 1] = shortest((int**)array, size_x, size_y, back_x, back_y);
-		} else if(way[i] == 'D') {
+		} else if (way[i] == 'D') {
 			back_x = back_x + 1;
 			way[i + 1] = shortest((int**)array, size_x, size_y, back_x, back_y);
 		} else {
 			way[i + 1] = '\0';
 		}
-		i++;
-	} while(i < size_x * size_y);
+	}
 	normWayArray(way, sizeWay);
 }
 void normWayArray(char *way, int size)	// normalize 'way' array
 {
 	const int tempSize = size;
 	char temp[tempSize];
-	int i = 0, j = 0, min_n;
+	int i, j, min_n;
 
-	do {
-		if(way[i] == 'R' || way[i] == 'L' || way[i] == 'U' || way[i] == 'D') {
-			i++;
-		} else
+	for (i = 0, j = 0; i < size; i++) {	// search minimal n
+		if (way[i] == 'R' || way[i] == 'L' || way[i] == 'U' || way[i] == 'D') {
+			continue;
+		} else {
 			break;
-	} while(i < size);
-
+		}
+	}
 	min_n = i - 2;
-	i = 0;
-	j = min_n;
 
-	do {	// reverse 'way' array, write result in 'temp' array
+	// reverse 'way' array, write result in 'temp' array
+	for (i = 0, j = min_n; j >= 0; i++, j--) {
 		temp[i] = way[j];
-		i++;
-		j--;
-	} while (j > 0);
-
-	i = 0;
-
-	do {	// 'way' array = 'temp' array
+	}
+	for (i = 0; i <= min_n; i++) {	// 'way' array = 'temp' array
 		way[i] = temp[i];
-		i++;
-	} while (i < min_n);
-
-	i = 0;
-
-	do {	// invert Right, Left, Up, Down
-		if(way[i] == 'R') {
+	}
+	for( i = 0; i <= size; i++) {	// invert Right, Left, Up, Down
+		if (way[i] == 'R') {
 			way[i] = 'L';
-			i++;
 			continue;
-		} else if(way[i] == 'L') {
+		} else if (way[i] == 'L') {
 			way[i] = 'R';
-			i++;
 			continue;
-		} else if(way[i] == 'U') {
+		} else if (way[i] == 'U') {
 			way[i] = 'D';
-			i++;
 			continue;
-		} else if(way[i] == 'D') {
+		} else if (way[i] == 'D') {
 			way[i] = 'U';
-			i++;
 			continue;
-		} else
+		} else {
 			way[i] = '\0';
-		i++;
-	} while(i < size);
-	i = min_n;
-	do {	// clean 'way' array of trash
+		}
+	}
+	for (i = min_n; i <= size - min_n; i++) {	// clean 'way' array of trash
 		way[i] = '\0';
-		i++;
-	} while (i <= size - (min_n));
+	}
 }
